7356b8ecc5f3475ffc67aeb71483770b7036f352
Sylvain Legault <slegault@aldogroup.com>
2025-12-31 08:25:48 -0500

    name

--- main/garage.h
+++ main/garage.h
@@ -29,8 +29,9 @@
 
 /* Basic manufacturer information */
 #define ESP_MANUFACTURER_NAME "\x09" \
-                              "ESPRESSIF"             /* Customized manufacturer name */
-#define ESP_MODEL_IDENTIFIER "\x07" CONFIG_IDF_TARGET /* Customized model identifier */
+                              "ESPRESSIF" /* Customized manufacturer name */
+#define ESP_MODEL_IDENTIFIER "\x07" \
+                             "esp32c6" /* Customized model identifier */
 
 #define ESP_ZB_ZED_CONFIG()                               \
     {                                                     \
